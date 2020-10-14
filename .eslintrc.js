749c1b40b004608e8255b46e96fefa791ee610fc
James Willcox <snorp@snorp.net>
2020-10-14 15:14:51 -0500

    Update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es6": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-constant-condition": ["error", { "checkLoops": false }],
-        "eol-last": ["error", "always"],
-        "class-methods-use-this": ["error"],
-        "indent": ["error", 2],
-        "brace-style": ["error"],
-        "key-spacing": ["error"],
-        "keyword-spacing": ["error"],
-        "object-curly-spacing": ["error", "always"],
-        "object-curly-newline": ["error"],
-        "no-console": "off",
-    }
+  "env": {
+    "node": true,
+    "es6": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-constant-condition": ["error", { "checkLoops": false }],
+    "eol-last": ["error", "always"],
+    "class-methods-use-this": ["error"],
+    "indent": ["error", 2],
+    "brace-style": ["error"],
+    "key-spacing": ["error"],
+    "keyword-spacing": ["error"],
+    "object-curly-spacing": ["error", "always"],
+    "object-curly-newline": ["error"],
+    "no-console": "off",
+  }
 };
